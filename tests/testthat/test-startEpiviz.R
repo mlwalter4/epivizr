9ec29a50e54a13398c670ce9be50368159094a54
d.tenenbaum <d.tenenbaum@bc3139a8-67e5-0310-9ffc-ced21a209358>
2016-05-03 21:26:31 +0000

    Creating branch for BioC 3.3 release.
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/epivizr@117080 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ tests/testthat/test-startEpiviz.R
@@ -0,0 +1,14 @@
+context("start epiviz")
+
+test_that("startEpiviz creates a proper object", {
+  skip("gc error")
+  
+  app <- startEpiviz(non_interactive=TRUE)
+  expect_is(app, "EpivizApp")
+  
+  expect_is(app$server, "EpivizServer")
+  expect_is(app$chart_mgr, "EpivizChartMgr")
+  expect_is(app$data_mgr, "EpivizDataMgr")
+  
+  expect_true(app$server$is_closed())
+})
